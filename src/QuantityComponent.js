84b5e89014eb22325dc0f97bd8c3d2a65e652bc0
Pathum Weerathunga <pathummadushanka.weerathunga@student.kyh.se>
2021-10-21 23:51:23 +0200

    When user clicks - btn when the Q is 1, delete item check

--- src/QuantityComponent.js
+++ src/QuantityComponent.js
@@ -1,20 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
-    /* display: flex; */
-    
     input{
         width: 30px;
+        text-align: center;
     }
 `
 
 export default function QuantityComponent() {
+
+    const [number, setNumber] = useState(1);
+    const [quantityType, setQuantityType] = useState('st');
+
+    const handlePlus = () => {
+        setNumber(number + 1);
+    }
+    const handleMinus = () => {
+        if (number === 1) {
+            alert("Vänta..want to delete item")
+        } else {  //When user click the (-) button, delete the item when the Q is = 0
+            setNumber(number - 1);
+        }
+
+
+    }
+
+
+
     return (
         <Container>
-            <button>-</button>
-            <input type="text" />
-            <button>+</button>
+            <button onClick={handleMinus}>-</button>
+            <input type="text" value={number} />
+            <button onClick={handlePlus}>+</button>
+            <span>{quantityType}</span>
         </Container>
     )
 }
